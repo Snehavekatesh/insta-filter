343c0f72c5d8d3c2f64a8461d6847cbbcb8abeca
Sneha <snehavenkatesh10@gmail.com>
2019-07-17 20:07:19 +0530

    add comments

--- instafilter.py
+++ instafilter.py
@@ -1,13 +1,14 @@
+# Import Image from PIL
 from PIL import Image
-
+#load images into objects 
 base_img= Image.open("potrait.jpg")
 img_filter= Image.open("download.jpeg") #opening image
-
+# set o/p image size
 size=(760,760)
-
+#resize all images to o/p size
 base_img= base_img.resize(size) #for resizing
 img_filter=img_filter.resize(size)
-
+#split each image into rgb
 r,g,b = base_img.split()
 R,G,B= img_filter.split()
 
