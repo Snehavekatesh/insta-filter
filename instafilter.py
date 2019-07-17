d42377724c83a0e5c9890497475843e654bb0495
Sneha <snehavenkatesh10@gmail.com>
2019-07-17 20:09:49 +0530

    remove second image

--- instafilter.py
+++ instafilter.py
@@ -2,15 +2,15 @@
 from PIL import Image
 #load images into objects 
 base_img= Image.open("potrait.jpg")
-img_filter= Image.open("download.jpeg") #opening image
+
 # set o/p image size
 size=(760,760)
 #resize all images to o/p size
 base_img= base_img.resize(size) #for resizing
-img_filter=img_filter.resize(size)
+
 #split each image into rgb
 r,g,b = base_img.split()
-R,G,B= img_filter.split()
+
 #to merge all the pictures
 im=Image.merge("RGB", (r, g, B))
 im.show()
